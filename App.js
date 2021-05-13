1e37435c9e49ea4238eb311a40dd1d7cffb8920b
nitin dodeja <nitin.d@indianic.com>
2021-05-13 15:27:48 +0530

    space

--- App.js
+++ App.js
@@ -106,7 +106,7 @@ const App: () => Node = () => {
           <Section title="Press to invoke native module">
             <TouchableOpacity onPress={()=>{
               if(Platform.OS === 'android'){
-                onPress();
+                onPress()
               }
             }}> 
 
